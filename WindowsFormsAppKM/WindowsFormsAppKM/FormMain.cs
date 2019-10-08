fedb0b3cea26375f16d6c3531c3455f3154ad5f3
Filip <filip.k.wanat@gmail.com>
2019-10-08 23:15:01 +0200

    Improve structure and UI

--- WindowsFormsAppKM/WindowsFormsAppKM/FormMain.cs
+++ WindowsFormsAppKM/WindowsFormsAppKM/FormMain.cs
@@ -10,15 +10,18 @@ using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsAppKM.Interfaces;
 
 namespace WindowsFormsAppKM
 {
     public partial class FormMain : Form
     {
+        private iPlayer Player { get; set; }
 
         public FormMain()
         {
             InitializeComponent();
+            comboBoxPlayMethod.SelectedIndex = 0;
         }
 
         private void SetText(string text)
@@ -26,14 +29,53 @@ namespace WindowsFormsAppKM
             textBoxSelectedFile.Text = text;
         }
 
+        private void ChangeFileName(string fileName)
+        {
+            Player = CreatePlayer(fileName);
+            if(Player == null) return;
+            SetText(fileName);
+            buttonPause.Enabled = Player.IsPausable;
+        }
+
+        private iPlayer CreatePlayer(string fileName)
+        {
+            switch (comboBoxPlayMethod.Text)
+            {
+                case "Play Sound":
+                    return new PlaySoundPlayer(fileName);
+                default:
+                    return null;
+            }
+        }
+
         private void buttonFile_Click(object sender, EventArgs e)
         {
-            SetText(openFileDialog.FileName);
+            Player?.Stop();
+            openFileDialog.ShowDialog();
+            if (!openFileDialog.CheckFileExists) return;
+            ChangeFileName(openFileDialog.FileName);
         }
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
-            PlaySoundPlayer.Play(openFileDialog.FileName);
+            Player?.Play();
+        }
+
+        private void ButtonPause_Click(object sender, EventArgs e)
+        {
+            Player?.Pause();
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            Player?.Stop();
+        }
+
+        private void ComboBoxPlayMethod_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Player?.Stop();
+            if (openFileDialog.FileName == "" || !openFileDialog.CheckFileExists) return;
+            Player = CreatePlayer(openFileDialog.FileName);
         }
     }
 }
---
+++ WindowsFormsAppKM/WindowsFormsAppKM/Interfaces/iPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppKM.Interfaces
+{
+    interface iPlayer
+    {
+        string FileName { get; }
+        bool IsPausable { get; }
+
+        void Play();
+        void Pause();
+        void Stop();
+    }
+}
